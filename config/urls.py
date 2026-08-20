88ba07bb36a1f7b9f4a82b6fa99fd2f46b730f3f
DARELLE <darellekaptue@gmail.com>
2026-08-20 17:58:58 +0100

    modification

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import home,professeurs,contact
+from core.views import home,professeurs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
     path('professeurs/', professeurs) ,
-    path('contact/', contact),
+    
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,43 @@
 from django.shortcuts import render
-from django .http import HttpResponse
+#from django .http import HttpResponse
 
 # Create your views here.
 def home(request): 
     return HttpResponse('Bienvenue sur ACADEMY')
+# def professeurs(request):
+   # return HttpResponse('voici nos professeurs')
 def professeurs(request):
-    return HttpResponse('voici nos professeurs')
-def contact(request):
-    return HttpResponse('Contacter ACADEMY')
+    professeurs = [
+        {
+            "nom": "Marie",
+            "matiere": "Mathématiques",
+            "tarif": 4800
+        },
+        {
+            "nom": "Jean",
+            "matiere": "Physique",
+            "tarif": 4500
+        },
+        {
+            "nom": "Pierre",
+            "matiere": "Chimie",
+            "tarif": 4000
+        },
+        {
+            "nom": "Lucie",
+            "matiere": "Biologie",
+            "tarif": 4200
+        },
+        {
+            "nom": "Sophie",
+            "matiere": "Histoire",
+            "tarif": 3800
+        },
+        {
+            "nom": "Antoine",
+            "matiere": "Géographie",
+            "tarif": 3500
+        }
+    ]
+    contexte = {'professeurs': professeurs}
+    return render(request, 'Core/professeurs.html', contexte)
